b7d22821097ef31f9532b360cbfaead93dfa8c64
Ananya-Sundararajan <axs2175@case.edu>
2026-04-10 20:52:08 -0700

    added functionality so multiple reconstructed folders can be uploaded at the same time

--- src/glomerular_segment_matcher/_reader.py
+++ src/glomerular_segment_matcher/_reader.py
@@ -1,20 +1,20 @@
-import numpy as np
 import os
-import warnings
 from pathlib import Path
-import tifffile as tiff
-from natsort import natsorted
+
 import dask
-import imageio.v3 as iio
 import dask.array as da
+import imageio.v3 as iio
+import numpy as np
+import tifffile as tiff
 from napari.utils import progress
+from natsort import natsorted
 
 IMAGE_FOLDER_NAME = 'images'
 MASK_FOLDER_NAME = 'masks'
 RECON_FOLDER_NAME = 'reconstructed'
 
-def napari_get_reader(path):
 
+def napari_get_reader(path):
     """
     Parameters
     ----------
@@ -24,8 +24,8 @@ def napari_get_reader(path):
     Returns
     -------
     function or None
-        If the path is a directory of two folders of images and masks in format 
-        .tif or .png, return a function that accepts the same path or list of paths, 
+        If the path is a directory of two folders of images and masks in format
+        .tif or .png, return a function that accepts the same path or list of paths,
         and returns a list of layer data tuples.
     """
 
@@ -47,47 +47,59 @@ def reader_function(path):
     layer_data : list of tuples
         A list of LayerData tuples where each tuple in the list contains
         (data, metadata, layer_type)
-            data: numpy array, 
+            data: numpy array,
             metadata: dict,
             layer_type: string
     """
-    
+
     path = Path(path)
     all_layer_data = []
 
     # method to read in images (works for both tifs and pngs)
     @dask.delayed
     def read_image(im_path):
-
         img = iio.imread(im_path)
         img = np.asarray(img)
         img = np.squeeze(img)
 
-        if im_path.endswith(".tif"):
-            if img.ndim == 3:
-                # channel-last
-                if img.shape[-1] in (3, 4):
-                    img = img[..., 0]
-                # channel-first
-                elif img.shape[0] in (3, 4):
-                    img = img[0, ...]
+        if im_path.endswith('.tif') and img.ndim == 3:
+            # channel-last
+            if img.shape[-1] in (3, 4):
+                img = img[..., 0]
+            # channel-first
+            elif img.shape[0] in (3, 4):
+                img = img[0, ...]
 
         if img.ndim < 2:
-            raise ValueError(f"{path.name} has unsupported shape {img.shape}. "
-                             "Only 2D or RGB images are supported.")
+            raise ValueError(
+                f'{path.name} has unsupported shape {img.shape}. '
+                'Only 2D or RGB images are supported.'
+            )
         return img
 
-    # read in the reconstruction file paths (does not have to be inputted)
-    recon_dir = path / RECON_FOLDER_NAME
-    if recon_dir.exists():
+    # Find all directories starting with 'reconstructed'
+    recon_dirs = [
+        d
+        for d in path.iterdir()
+        if d.is_dir() and d.name.startswith(RECON_FOLDER_NAME)
+    ]  # Added logic to find all matching folders
+    num_recon = 0  # Initialize counter to support validation later
+
+    for (
+        recon_dir
+    ) in recon_dirs:  # added loop to process multiple recon folders
         recon_file_names = natsorted(os.listdir(recon_dir))
-        recon_file_paths = [os.path.join(recon_dir, f) for f in recon_file_names if f.endswith(".tif") or f.endswith(".png")]
+        recon_file_paths = [
+            os.path.join(recon_dir, f)
+            for f in recon_file_names
+            if f.endswith(['.tif', '.png'])
+        ]
 
         num_recon = len(recon_file_paths)
         # read in one reconstructed mask to get data type and shape
         recon_shape = None
         recon_dtype = None
-        if recon_file_names[0].endswith(".tif"):
+        if recon_file_names[0].endswith('.tif'):
             recon = tiff.imread(recon_file_paths[0])
             recon_shape = recon.shape
             recon_dtype = recon.dtype
@@ -96,34 +108,47 @@ def reader_function(path):
             recon_shape = recon.shape
             recon_dtype = recon.dtype
 
-        recon_stack = [da.zeros(shape=recon_shape, dtype=recon_dtype) for _ in range(num_recon)]
-        
+        recon_stack = [
+            da.zeros(shape=recon_shape, dtype=recon_dtype)
+            for _ in range(num_recon)
+        ]
+
         # create the stack with appropriate data for reconstructed masks
         for i in progress(range(len(recon_file_paths))):
             recon_path = recon_file_paths[i]
-            recon = da.from_delayed(read_image(recon_path), shape=recon_shape, dtype=recon_dtype)
+            recon = da.from_delayed(
+                read_image(recon_path), shape=recon_shape, dtype=recon_dtype
+            )
             recon_stack[i] = recon
 
         recon_layer_data = da.stack(recon_stack)
-        recon_layer_type = "labels"
-        recon_layer_name = RECON_FOLDER_NAME
-        recon_add_kwargs = {"name": recon_layer_name}
+        recon_layer_type = 'labels'
+        recon_layer_name = (
+            recon_dir.name
+        )  # changed to use the specific folder name for the layer
+        recon_add_kwargs = {'name': recon_layer_name}
 
-        all_layer_data.append((recon_layer_data, recon_add_kwargs, recon_layer_type))
+        all_layer_data.append(
+            (recon_layer_data, recon_add_kwargs, recon_layer_type)
+        )
 
     # load in image file paths
     image_dir = path / IMAGE_FOLDER_NAME
     if not image_dir.exists():
-        warnings.warn("Valid image directory is not given.")
+        raise ValueError('Valid image directory is not given.')
     image_file_names = natsorted(os.listdir(image_dir))
-    image_file_paths = [os.path.join(image_dir, f) for f in image_file_names if f.endswith(".tif") or f.endswith(".png")]
-    
+    image_file_paths = [
+        os.path.join(image_dir, f)
+        for f in image_file_names
+        if f.endswith(['.tif', '.png'])
+    ]
+
     num_images = len(image_file_paths)
 
     # read in one image to get data type and shape
     im_shape = None
     im_dtype = None
-    if image_file_names[0].endswith(".tif"):
+    if image_file_names[0].endswith('.tif'):
         im = tiff.imread(image_file_paths[0])
         im_shape = im.shape
         im_dtype = im.dtype
@@ -135,15 +160,19 @@ def reader_function(path):
     # read in mask file paths
     mask_dir = path / MASK_FOLDER_NAME
     if not mask_dir.exists():
-        warnings.warn("Valid mask directory is not given.")
+        raise ValueError('No mask directory has been given.')
     mask_file_names = natsorted(os.listdir(mask_dir))
-    mask_file_paths = [os.path.join(mask_dir, f) for f in mask_file_names if f.endswith(".tif") or f.endswith(".png")]
+    mask_file_paths = [
+        os.path.join(mask_dir, f)
+        for f in mask_file_names
+        if f.endswith(['.tif', '.png'])
+    ]
 
     num_masks = len(mask_file_paths)
     # read in one mask to get data type and shape
     mask_shape = None
     mask_dtype = None
-    if mask_file_names[0].endswith(".tif"):
+    if mask_file_names[0].endswith('.tif'):
         mask = tiff.imread(mask_file_paths[0])
         mask_shape = mask.shape
         mask_dtype = mask.dtype
@@ -154,38 +183,54 @@ def reader_function(path):
 
     # if mask shape and image shape are not the same then raise error
     if num_masks != num_images:
-        raise ValueError(f"{num_images} images found, but {num_masks} masks found.")
-    if num_recon != num_images:
-            raise ValueError(f"{num_recon} reconstructed masks found, but {num_images} masks found.")
-    
+        raise ValueError(
+            f'{num_images} images found, but {num_masks} masks found.'
+        )
+    if (
+        len(recon_dirs) > 0 and num_recon != num_images
+    ):  # Updated to check if recons exist before comparing
+        raise ValueError(
+            f'{num_recon} reconstructed masks found, but {num_images} masks found.'
+        )
+
     # initialize stack size
-    image_stack = [da.zeros(shape=im_shape, dtype=im_dtype) for _ in range(num_images)]
-    mask_stack = [da.zeros(shape=mask_shape, dtype=mask_dtype) for _ in range(num_masks)]
-    
+    image_stack = [
+        da.zeros(shape=im_shape, dtype=im_dtype) for _ in range(num_images)
+    ]
+    mask_stack = [
+        da.zeros(shape=mask_shape, dtype=mask_dtype) for _ in range(num_masks)
+    ]
+
     # create the stack with appropriate data for images
     for i in progress(range(len(image_file_paths))):
         image_path = image_file_paths[i]
-        image = da.from_delayed(read_image(image_path), shape=im_shape, dtype=im_dtype)
+        image = da.from_delayed(
+            read_image(image_path), shape=im_shape, dtype=im_dtype
+        )
         image_stack[i] = image
 
     image_layer_data = da.stack(image_stack)
-    image_layer_type = "image"
+    image_layer_type = 'image'
     image_layer_name = IMAGE_FOLDER_NAME
-    image_add_kwargs = {"name": image_layer_name}
+    image_add_kwargs = {'name': image_layer_name}
 
     # create the stack with appropriate data for masks
     for i in progress(range(len(mask_file_paths))):
         mask_path = mask_file_paths[i]
-        mask = da.from_delayed(read_image(mask_path), shape=mask_shape, dtype=mask_dtype)
+        mask = da.from_delayed(
+            read_image(mask_path), shape=mask_shape, dtype=mask_dtype
+        )
         mask_stack[i] = mask
 
     mask_layer_data = da.stack(mask_stack)
-    mask_layer_type = "labels"
+    mask_layer_type = 'labels'
     mask_layer_name = MASK_FOLDER_NAME
-    mask_add_kwargs = {"name": mask_layer_name}
+    mask_add_kwargs = {'name': mask_layer_name}
 
     # append both image layer and mask layer
-    all_layer_data.append((image_layer_data, image_add_kwargs, image_layer_type))
+    all_layer_data.append(
+        (image_layer_data, image_add_kwargs, image_layer_type)
+    )
     all_layer_data.append((mask_layer_data, mask_add_kwargs, mask_layer_type))
 
     return all_layer_data
--- src/glomerular_segment_matcher/_widget.py
+++ src/glomerular_segment_matcher/_widget.py
@@ -1,17 +1,25 @@
 from typing import TYPE_CHECKING
 
-from qtpy.QtWidgets import QLabel, QPushButton, QWidget, QVBoxLayout, QHBoxLayout
 import numpy as np
-from qtpy.QtCore import QTimer, Qt
-
+from qtpy.QtCore import Qt, QTimer
+from qtpy.QtWidgets import (
+    QHBoxLayout,
+    QLabel,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
 
 if TYPE_CHECKING:
     import napari
 
+RECON_FOLDER_NAME = 'reconstructed'
+
+
 class SegmentMatcher(QWidget):
     def __init__(self, viewer: 'napari.viewer.Viewer'):
         super().__init__()
-        
+
         self.viewer = viewer
         self.viewer.grid.enabled = True
         self.viewer.grid.shape = (1, 2)
@@ -21,18 +29,19 @@ class SegmentMatcher(QWidget):
         self.right_img_layer = None
         self.left_mask_layer = None
         self.right_mask_layer = None
-        self.left_recon_layer = None
-        self.right_recon_layer = None
+
+        self.left_recon_layers = {}
+        self.right_recon_layers = {}
 
         # buttons
-        self.prev_btn = QPushButton("< Previous")
-        self.next_btn = QPushButton("Next >")
-        self.save_changes = QPushButton("Save Changes")
+        self.prev_btn = QPushButton('< Previous')
+        self.next_btn = QPushButton('Next >')
+        self.save_changes = QPushButton('Save Changes')
         self.prev_btn.clicked.connect(self.on_prev)
         self.next_btn.clicked.connect(self.on_next)
         self.save_changes.clicked.connect(self.on_save)
 
-        self.label = QLabel("Matching: Slices 0 and 1")
+        self.label = QLabel('Matching: Slices 0 and 1')
         self.label.setAlignment(Qt.AlignCenter)
 
         # layouts
@@ -59,16 +68,28 @@ class SegmentMatcher(QWidget):
     def _maybe_initialize(self, event=None):
         if 'images' in self.viewer.layers and 'masks' in self.viewer.layers:
             # prevent running multiple times
-            self.viewer.layers.events.inserted.disconnect(self._maybe_initialize)
+            self.viewer.layers.events.inserted.disconnect(
+                self._maybe_initialize
+            )
             self.update_view()
 
     def update_view(self):
-        if 'images' not in self.viewer.layers or 'masks' not in self.viewer.layers:
+        if (
+            'images' not in self.viewer.layers
+            or 'masks' not in self.viewer.layers
+        ):
             return
-        
-        if 'reconstructed' in self.viewer.layers:
-            self.viewer.layers['reconstructed'].visible = False
-            recon_stack = self.viewer.layers['reconstructed'].data
+
+        # identify all base stacks from the reader
+        recon_base_layers = [
+            layer
+            for layer in self.viewer.layers
+            if layer.name.startswith(RECON_FOLDER_NAME)
+            and 'Pos' not in layer.name
+        ]
+
+        for r_layer in recon_base_layers:
+            r_layer.visible = False
 
         img_stack = self.viewer.layers['images'].data
         mask_stack = self.viewer.layers['masks'].data
@@ -76,139 +97,182 @@ class SegmentMatcher(QWidget):
         self.viewer.layers['masks'].visible = False
 
         i = self.idx
-
         left_img = img_stack[i]
         right_img = img_stack[i + 1]
-
         left_mask = np.asarray(mask_stack[i])
         right_mask = np.asarray(mask_stack[i + 1])
 
-        if 'reconstructed' in self.viewer.layers:
-            left_recon = np.asarray(recon_stack[i])
-            right_recon = np.asarray(recon_stack[i + 1])
-
         self.update_labels()
 
+        # image layer handling
         if self.left_img_layer is None:
-            self.right_img_layer = self.viewer.add_image(right_img, name=f"Pos {i + 1} Image") #translate=(0, 1)
-            self.left_img_layer = self.viewer.add_image(left_img, name=f"Pos {i} Image")
+            self.right_img_layer = self.viewer.add_image(
+                right_img, name=f'Pos {i + 1} Image'
+            )
+            self.left_img_layer = self.viewer.add_image(
+                left_img, name=f'Pos {i} Image'
+            )
         else:
             self.right_img_layer.data = right_img
             self.left_img_layer.data = left_img
-            self.right_img_layer.name = f"Pos {i + 1} Image"
-            self.left_img_layer.name = f"Pos {i} Image"
-        
-        if 'reconstructed' in self.viewer.layers:
-            if self.left_recon_layer is None:
-                self.right_recon_layer = self.viewer.add_labels(right_recon, name=f"Pos {i + 1} Reconstructed") # translate=(0, 1
-                self.right_recon_layer.visible = False
-                self.left_recon_layer = self.viewer.add_labels(left_recon, name=f"Pos {i} Reconstructed")
-                self.left_recon_layer.visible = False
-            else:
-                self.right_recon_layer.data = right_recon
-                self.right_recon_layer.visible = False
-                self.left_recon_layer.data = left_recon
-                self.left_recon_layer.visible = False
-                self.right_recon_layer.name = f"Pos {i + 1} Recon"
-                self.left_recon_layer.name = f"Pos {i} Recon"
+            self.right_img_layer.name = f'Pos {i + 1} Image'
+            self.left_img_layer.name = f'Pos {i} Image'
 
+        # reconstruction layer handling
+        for (
+            r_layer
+        ) in recon_base_layers:  # loop through all base reconstruction stacks
+            r_data = r_layer.data
+            l_data = np.asarray(r_data[i])
+            rs_data = np.asarray(r_data[i + 1])
+
+            suffix = r_layer.name.replace(RECON_FOLDER_NAME, '')
+
+            # check if we already created view layers for this specific suffix
+            if suffix not in self.left_recon_layers:
+                self.right_recon_layers[suffix] = self.viewer.add_labels(
+                    rs_data, name=f'Pos {i + 1} Recon{suffix}'
+                )
+                self.right_recon_layers[suffix].visible = False
+                self.left_recon_layers[suffix] = self.viewer.add_labels(
+                    l_data, name=f'Pos {i} Recon{suffix}'
+                )
+                self.left_recon_layers[suffix].visible = False
+            else:
+                self.right_recon_layers[suffix].data = rs_data
+                self.left_recon_layers[suffix].data = l_data
+                self.right_recon_layers[
+                    suffix
+                ].name = f'Pos {i + 1} Recon{suffix}'
+                self.left_recon_layers[suffix].name = f'Pos {i} Recon{suffix}'
+
+        # mask layer handling
         if self.left_mask_layer is None:
-            self.right_mask_layer = self.viewer.add_labels(right_mask, name=f"Pos {i + 1} Mask") #translate=(0, 1)
-            self.right_mask_layer.mouse_drag_callbacks.append(self._after_matching)
-            self.left_mask_layer = self.viewer.add_labels(left_mask, name=f"Pos {i} Mask")
-            self.left_mask_layer.mouse_drag_callbacks.append(self._transfer_label)
+            self.right_mask_layer = self.viewer.add_labels(
+                right_mask, name=f'Pos {i + 1} Mask'
+            )
+            self.right_mask_layer.mouse_drag_callbacks.append(
+                self._after_matching
+            )
+            self.left_mask_layer = self.viewer.add_labels(
+                left_mask, name=f'Pos {i} Mask'
+            )
+            self.left_mask_layer.mouse_drag_callbacks.append(
+                self._transfer_label
+            )
         else:
             self.right_mask_layer.data = right_mask
             self.left_mask_layer.data = left_mask
-            self.right_mask_layer.name = f"Pos {i + 1} Mask"
-            self.left_mask_layer.name = f"Pos {i} Mask"
+            self.right_mask_layer.name = f'Pos {i + 1} Mask'
+            self.left_mask_layer.name = f'Pos {i} Mask'
 
         QTimer.singleShot(0, self._set_active)
+        self.label.setText(f'Matching: Slices {self.idx} and {self.idx + 1}')
 
-        # update label
-        self.label.setText(f"Matching: Slices {self.idx} and {self.idx + 1}")
-        
     def _set_active(self):
         self.viewer.layers.selection.clear()
         self.viewer.layers.selection.active = self.left_mask_layer
         self.left_mask_layer.mode = 'pan_zoom'
 
-        # need to rename here as well because of timing issues in updates
         i = self.idx
-        self.right_mask_layer.name = f"Pos {i + 1} Mask"
-        self.left_mask_layer.name = f"Pos {i} Mask"
-        self.right_recon_layer.name = f"Pos {i + 1} Recon"
-        self.left_recon_layer.name = f"Pos {i} Recon"
-        self.right_img_layer.name = f"Pos {i + 1} Image"
-        self.left_img_layer.name = f"Pos {i} Image"
+        self.right_mask_layer.name = f'Pos {i + 1} Mask'
+        self.left_mask_layer.name = f'Pos {i} Mask'
+
+        for suffix, layer in self.left_recon_layers.items():
+            layer.name = f'Pos {i} Recon{suffix}'
+        for suffix, layer in self.right_recon_layers.items():
+            layer.name = f'Pos {i + 1} Recon{suffix}'
+
+        self.right_img_layer.name = f'Pos {i + 1} Image'
+        self.left_img_layer.name = f'Pos {i} Image'
 
     def update_labels(self):
-        label_pos = np.array([[-30, 0]]) 
+        label_pos = np.array([[-30, 0]])
 
         # --- RIGHT LABEL ---
         if 'UI_Label_Right' in self.viewer.layers:
             layer_r = self.viewer.layers['UI_Label_Right']
             layer_r.data = label_pos
-            layer_r.features = {'txt': [f"Slice {self.idx + 1}"]}
+            layer_r.features = {'txt': [f'Slice {self.idx + 1}']}
             layer_r.refresh_text()
-            #layer_r.grid_index = (0, 1)
+            # layer_r.grid_index = (0, 1)
         else:
             layer_r = self.viewer.add_points(
-                label_pos, name='UI_Label_Right',
-                features={'txt': [f"Slice {self.idx + 1}"]},
-                text={'string': '{txt}', 'size': 20, 'color': 'white', 'anchor': 'upper_left'},
-                size=0, face_color='transparent', border_color='transparent'
+                label_pos,
+                name='UI_Label_Right',
+                features={'txt': [f'Slice {self.idx + 1}']},
+                text={
+                    'string': '{txt}',
+                    'size': 20,
+                    'color': 'white',
+                    'anchor': 'upper_left',
+                },
+                size=0,
+                face_color='transparent',
+                border_color='transparent',
             )
-            #layer_r.grid_index = (0, 1) # Force Right
+            # layer_r.grid_index = (0, 1) # Force Right
 
-        
         # --- LEFT LABEL ---
         if 'UI_Label_Left' in self.viewer.layers:
             layer_l = self.viewer.layers['UI_Label_Left']
             layer_l.data = label_pos
-            layer_l.features = {'txt': [f"Slice {self.idx}"]}
+            layer_l.features = {'txt': [f'Slice {self.idx}']}
             layer_l.refresh_text()
-            #layer_l.grid_index = (0, 0)
+            # layer_l.grid_index = (0, 0)
         else:
             layer_l = self.viewer.add_points(
-                label_pos, name='UI_Label_Left',
-                features={'txt': [f"Slice {self.idx}"]},
-                text={'string': '{txt}', 'size': 20, 'color': 'white', 'anchor': 'upper_left'},
-                size=0, face_color='transparent', border_color='transparent'
+                label_pos,
+                name='UI_Label_Left',
+                features={'txt': [f'Slice {self.idx}']},
+                text={
+                    'string': '{txt}',
+                    'size': 20,
+                    'color': 'white',
+                    'anchor': 'upper_left',
+                },
+                size=0,
+                face_color='transparent',
+                border_color='transparent',
             )
-            #layer_l.grid_index = (0, 0)
+            # layer_l.grid_index = (0, 0)
 
     def _transfer_label(self, layer, event):
-
         # we dont want it to keep moving to the other layer if we didn't pick a label
         if layer.mode != 'pick':
             return
-        
+
         coords = layer.world_to_data(event.position)
         val = layer.get_value(coords)
 
         if val is not None:
             self.right_mask_layer.selected_label = val
-            
+
             self.viewer.layers.selection.active = self.right_mask_layer
-            
+
             self.right_mask_layer.mode = 'pan_zoom'
 
     def _after_matching(self, layer, event):
-
         if layer.mode != 'fill':
             return
-        
-        yield # wait for fill to be used
-        
+
+        yield  # wait for fill to be used
+
         self.viewer.layers.selection.active = self.left_mask_layer
         self.left_mask_layer.mode = 'pan_zoom'
 
     # --------------------- BUTTON HANDLERS ---------------------
     def on_next(self):
-        if self.left_mask_layer is not None and self.right_mask_layer is not None:
-            self.viewer.layers['masks'].data[self.idx] = self.left_mask_layer.data
-            self.viewer.layers['masks'].data[self.idx + 1] = self.right_mask_layer.data
+        if (
+            self.left_mask_layer is not None
+            and self.right_mask_layer is not None
+        ):
+            self.viewer.layers['masks'].data[self.idx] = (
+                self.left_mask_layer.data
+            )
+            self.viewer.layers['masks'].data[self.idx + 1] = (
+                self.right_mask_layer.data
+            )
 
         # beacuse we displaying 2 at a time, we want second to last image be maximum
         max_idx = self.viewer.layers['images'].data.shape[0] - 2
@@ -217,15 +281,29 @@ class SegmentMatcher(QWidget):
             self.update_view()
 
     def on_prev(self):
-        if self.left_mask_layer is not None and self.right_mask_layer is not None:
-            self.viewer.layers['masks'].data[self.idx] = self.left_mask_layer.data
-            self.viewer.layers['masks'].data[self.idx + 1] = self.right_mask_layer.data
+        if (
+            self.left_mask_layer is not None
+            and self.right_mask_layer is not None
+        ):
+            self.viewer.layers['masks'].data[self.idx] = (
+                self.left_mask_layer.data
+            )
+            self.viewer.layers['masks'].data[self.idx + 1] = (
+                self.right_mask_layer.data
+            )
 
         if self.idx > 0:
             self.idx -= 1
             self.update_view()
 
     def on_save(self):
-        if self.left_mask_layer is not None and self.right_mask_layer is not None:
-            self.viewer.layers['masks'].data[self.idx] = self.left_mask_layer.data
-            self.viewer.layers['masks'].data[self.idx + 1] = self.right_mask_layer.data
+        if (
+            self.left_mask_layer is not None
+            and self.right_mask_layer is not None
+        ):
+            self.viewer.layers['masks'].data[self.idx] = (
+                self.left_mask_layer.data
+            )
+            self.viewer.layers['masks'].data[self.idx + 1] = (
+                self.right_mask_layer.data
+            )
